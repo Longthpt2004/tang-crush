f75e71f6069e42597e2361586b33c7d8db9ed349
Longthpt2004 <86608160+Longthpt2004@users.noreply.github.com>
2021-07-02 20:29:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Babi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Linh <33',
-    btnNo: 'Không, Mày ảo quá Linh à =))',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Long <33',
+    btnNo: 'Không, Mày ảo quá Long à =))',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tôi <3',
-    btnReply: 'Gửi cho Linh <3',
-    reply: 'Thì tại Linh dễ thương quá chời <333',
-    mess: 'Tui biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay anh qua đón Linh đi chơi nhé <3.',
+    btnReply: 'Gửi cho Long <3',
+    reply: 'Thì tại Long đáng iu vl :333',
+    mess: 'Tui biết mà 🥰. Tui cũng yêu cậu lắm 😘😘',
+    messDesc: 'Tối nay Long qua đón babi đi chơi nhé <3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
